6cd3d9680d0332b2dec242af008edeb5c718481a
Felipe <felipe.presidencia@gmail.com>
2022-03-24 15:50:16 -0300

    C1 - Conexão com um servidor MongoDB

--- notas.js
+++ notas.js
@@ -7,4 +7,6 @@
  * para rodar o webpack.config.js
  * 
  * http://localhost:3000
+ * 
+ * criar o arquivo .env
  */
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conectado a base de dados MongoDB')
+    app.emit('pronto');
+  })
+  .catch(e => console.log(e));
+
 const routes = require('./routes');
 const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middlewares');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -10,10 +21,13 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+//Meus próprios middlewares
+app.use(middlewareGlobal);
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('🚀 Servidor Inicializado!');
-  console.log('Executando no link: http://localhost:3000');
+app.on('pronto', () => {
+  app.listen(3000, () => {
+    console.log('🚀 Servidor Inicializado!');
+    console.log('Executando no link: http://localhost:3000');
+  });
 });
-
