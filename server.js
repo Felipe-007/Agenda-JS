0d8cbfd8e4ad377dcefa151e3b01b69c1e645a1b
Felipe <felipe.presidencia@gmail.com>
2022-03-29 14:28:19 -0300

    C8 - ContatoModel, login-logado, contatoController

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTIONSTRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
   .then(() => {
-    console.log('Conectado a base de dados MongoDB')
     app.emit('pronto');
   })
   .catch(e => console.log(e));
-
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
-
 const routes = require('./routes');
 const path = require('path');
-const helmet = require('helmet');
+// const helmet = require('helmet'); // helmet começou a causar problemas no localhost por conta da falta de SSL
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-app.use(helmet());
+// app.use(helmet()); // helmet começou a causar problemas no localhost por conta da falta de SSL
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-  secret: 'adfadlçfsdoikloek',
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
   saveUninitialized: false,
   cookie: {
@@ -41,9 +43,8 @@ app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
-app.use(csrf()); //injeta token em todas as páginas
-
-//Meus próprios middlewares
+app.use(csrf());
+// Nossos próprios middlewares
 app.use(middlewareGlobal);
 app.use(checkCsrfError);
 app.use(csrfMiddleware);
@@ -51,7 +52,7 @@ app.use(routes);
 
 app.on('pronto', () => {
   app.listen(3000, () => {
-    console.log('🚀 Servidor Inicializado!');
-    console.log('Executando no link: http://localhost:3000');
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
   });
 });
